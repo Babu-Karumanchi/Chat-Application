eab1119dea292b355c3afbdb73fe60cd11f3a782
Babu Karumanchi <babukarumanchi1842002@gmail.com>
2025-08-06 10:58:07 +0530

    completed

---
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.chat.app.config;
+
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketMessagingAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+     registry.addEndpoint("/chat")
+             .setAllowedOrigins("htt[s://localhost:8080")
+             .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+
+    }
+}
